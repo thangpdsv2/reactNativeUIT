efa22e2ca85f32eeaf0dfde4c3b8d58d670b1adf
s4thomaspham <s4.thomaspham@wearesection.com>
2023-12-25 21:29:31 +0700

    update app

--- App.tsx
+++ App.tsx
@@ -6,27 +6,22 @@
  */
 import React from 'react';
 import {useState, useEffect} from 'react';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './components/HomeScreen';
-import TinhDiemTB from './components/TinhDiemTB';
-import Login from './components/Login';
 import ProfileScreen from './components/Profile';
-import FlatList from './components/FlatList';
-import SectionList from './components/SectionList';
-import Modal from './components/Modal';
 import Products from './pages/Products'
-import Flowers from './pages/Flowers'
 import SettingsScreen from './components/SettingsScreen'
 import Tab2Screen from './components/Tab2'
 import Tab1Screen from './components/Tab1'
+import {ApolloProvider,InMemoryCache } from '@apollo/client';
 import { persistCache } from 'apollo3-cache-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Loader from './components/Loader'
+import client from './utils/ctf-client'
 
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
@@ -49,14 +44,7 @@ const MainStack = () => (
 
 
 const cache = new InMemoryCache();
-const client = new ApolloClient({
-  uri: 'https://graphql.contentful.com/content/v1/spaces/zgbs11bm62wq',
-  cache,
-  credentials: 'same-origin',
-  headers: {
-    Authorization: `Bearer Qp_jaAUaoQ_S0jo_qUwlEuJETKHEhbyb5CbQ-33aWnE`,
-  },
-});
+
 function App(): JSX.Element {
   const [loadingCache, setLoadingCache] = useState(true)
   useEffect(() => {
---
+++ utils/ctf-client.ts
@@ -0,0 +1,15 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+
+
+
+
+const cache = new InMemoryCache();
+export default new ApolloClient({
+  uri: 'https://graphql.contentful.com/content/v1/spaces/zgbs11bm62wq',
+  cache,
+  credentials: 'same-origin',
+  headers: {
+    Authorization: `Bearer Qp_jaAUaoQ_S0jo_qUwlEuJETKHEhbyb5CbQ-33aWnE`,
+  },
+});
